6387c51b79f568a447969f298ec28bbb4d3cfb24
Giselle <gisellevonbingen@gmail.com>
2024-07-20 19:55:09 +0900

    Create

---
+++ common/src/main/java/ad_astra_giselle_addon/common/compat/create/CreateCommand.java
@@ -0,0 +1,25 @@
+package ad_astra_giselle_addon.common.compat.create;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.minecraft.commands.CommandSourceStack;
+
+public class CreateCommand
+{
+	public static int divingHelmet(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+	{
+		return 0;
+	}
+
+	public static int netheriteDivingHelmet(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+	{
+		return 0;
+	}
+
+	private CreateCommand()
+	{
+
+	}
+
+}
